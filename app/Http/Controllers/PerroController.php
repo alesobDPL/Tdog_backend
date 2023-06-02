85a0bd6c6fff630e4b277d67bd230c4f5a80f572
alesobDPL <alejandrosobarzo56@gmail.com>
2023-06-01 23:39:57 -0400

    nuevo commit

--- app/Http/Controllers/PerroController.php
+++ app/Http/Controllers/PerroController.php
@@ -2,9 +2,81 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Perro;
+use App\Http\Requests\PerroRequest;
 
 class PerroController extends Controller
 {
-    //
+
+    public function listar()
+    {
+        $perros = Perro::all();
+        return response()->json(["perros" => $perros], 200);
+    }
+
+
+    public function crear(PerroRequest $request)
+    {
+       try {
+            $perro = new Perro();
+            $perro->nombre = $request->nombre;
+            $perro->url_nombre = $request->url_nombre;
+            $perro->descripcion= $request->descripcion;
+            $perro->save();
+
+            return response()->json(["perro" => $perro], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json(["error" => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+
+
+    }
+
+    public function mostrar($id)
+    {
+        $perro = Perro::find($id);
+        if (!$perro) {
+            return response()->json(["error" => "Perro no encontrado"], 404);
+        }
+        return response()->json(["perro" => $perro], 200);
+    }
+
+    public function actualizar(PerroRequest $request, $id)
+    {
+        $perro = Perro::find($id);
+        if (!$perro) {
+            return response()->json(["error" => "Perro no encontrado"], 404);
+        }
+
+        try {
+            $perro->nombre = $request->nombre;
+            $perro->url_foto = $request->url_foto;
+            $perro->descripcion = $request->descripcion;
+            $perro->save();
+
+            return response()->json(["perro" => $perro], 200);
+        } catch (\Exception $e) {
+            return response()->json(["error" => $e->getMessage()], 400);
+        }
+    }
+
+
+    public function borrar($id)
+    {
+        $perro = Perro::find($id);
+        if (!$perro) {
+            return response()->json(["error" => "Perro no encontrado"], 404);
+        }
+
+        try {
+            $perro->delete();
+            return response()->json(["mensaje" => "Perro eliminado correctamente"], 200);
+        } catch (\Exception $e) {
+            return response()->json(["error" => $e->getMessage()], 400);
+        }
+    }
+
+
 }
---
+++ app/Http/Requests/PerroRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class PerroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function rules()
+    {
+        return [
+            "nombre" => "required|unique:pokemon|string",
+            "url_foto" => "required|max:120|string|unique",
+            "descripcion" => "required|max:300|string",
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'El campo :attribute es requerido',
+            'integer' => 'El campo :attribute debe ser un número entero',
+            'numeric' => 'El campo :attribute debe ser un número',
+            'exists' => 'El :attribute debe existir en nuestro sistema',
+            'boolean' => 'El campo :attribute debe ser un valor tipo boolean',
+            'string' => 'El campo : attribute debe ser de tipo string',
+            'unique' => 'El campo :attribute debe ser único en nuestro sistema',
+            'max' => 'El campo :attribute supera el largo máximo permitido',
+            'array' => 'El campo :attribute debe ser de tipo array'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors()->all(), Response::HTTP_BAD_REQUEST)
+        );
+    }
+    
+}
